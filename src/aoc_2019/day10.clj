6d4994f0e394b518cbece5675dae945732e190d8
Joshua Leger <legermjoshua@gmail.com>
2019-12-11 01:12:53 -0800

    Finished puzzle 2

--- src/aoc_2019/day10.clj
+++ src/aoc_2019/day10.clj
@@ -68,12 +68,30 @@
 
 (defn- remove-location [locations location] (remove #(= location %) locations))
 
-(defn- get-angle-between [reference other])
+(defn- get-atan [x y] (Math/atan (double (/ y x))))
+
+(defn- get-angle [location]
+  (let [x (:x location)
+        y (* (:y location) -1)]
+    (cond
+      (and (= x 0) (> y 0)) 0
+      (and (= x 0) (< y 0)) Math/PI
+      (and (= y 0) (< x 0)) (* 1.5 Math/PI)
+      (and (= y 0) (> x 0)) (* 0.5 Math/PI)
+      (> x 0) (- (/ Math/PI 2) (get-atan x y))
+      (< x 0) (+ Math/PI (- (/ Math/PI 2) (get-atan x y))))))
+
+(defn- get-angle-between [reference other]
+  (let [reference-angle (get-angle reference)
+        other-angle (get-angle other)]
+    (if (< reference-angle other-angle)
+      (- other-angle reference-angle)
+      (- (+ (* Math/PI 2) other-angle) reference-angle))))
 
 (defn- get-rotation-for-location [center other current-dir]
   (let [delta (get-delta-between-points center other)]
     (if (= delta current-dir)
-      360
+      (* 2 Math/PI)
       (get-angle-between current-dir delta))))
 
 (defn- get-next-destroyed [others center current-dir]
@@ -92,5 +110,6 @@
 (defn puzzle2 [input]
   (let [chart (to-chart input)
         station (:loc (get-most-visible chart))
-        locations (remove-location (get-locations chart) station)]
-    (get-x-destroyed locations station {:x -1 :y 1000} 199)))
+        locations (remove-location (get-locations chart) station)
+        last-destroyed(get-x-destroyed locations station {:x -1 :y -1000} 199)]
+    (+ (* (:x last-destroyed) 100) (:y last-destroyed))))
