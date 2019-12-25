eed38e6b8838332e205f4009bfb999e8823fdf01
Joshua Leger <legermjoshua@gmail.com>
2019-12-25 00:11:02 -0800

    Fully completed all challenges

--- src/aoc_2019/day25.clj
+++ src/aoc_2019/day25.clj
@@ -1,5 +1,24 @@
-(ns aoc-2019.day25)
+(ns aoc-2019.day25
+  (:require [aoc-2019.intcomp :as ic]))
 
-(defn puzzle1 [input])
+(defn- output-to-str [output]
+  (->> output
+       (map char)
+       (apply str)))
 
-(defn puzzle2 [input])
+(defn- command-to-input [command]
+  (->> command
+       (map char)
+       (map int)))
+
+(defn- command-robot [program command]
+  (let [new-program (ic/process-program-till-halt-or-input program command)]
+    (println (output-to-str (:output new-program)))
+    (if (:done new-program)
+      new-program
+      (recur new-program (command-to-input (str (read-line) "\n"))))))
+
+; You need cake, jam, easter egg, and asterisk to make it through
+(defn puzzle1 [input] (command-robot (ic/string-to-program input) []))
+
+(defn puzzle2 [input] "Contrats on finishing!")
